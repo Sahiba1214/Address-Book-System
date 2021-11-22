c6555ddfe1770ec238c07ac456ca2215e3e1452f
sahiba <sahiba1214@gmail.com>
2021-11-23 00:29:27 +0530

    [sahiba1214] add : file added

--- AddressBookSystem/AddressBookSystem/StartContact.cs
+++ AddressBookSystem/AddressBookSystem/StartContact.cs
@@ -35,6 +35,18 @@ namespace AddressBookSystem
                 }
             }
         }
+
+        public void DeleteContact(string user) // This Method Will Delete Particular Contact Detail Provided by User
+        {
+            Contact delete = new Contact();
+            foreach (var contact in addressList)
+            {
+                if (contact.FirstName == user || contact.LastName == user)
+                {
+                    addressList.Remove(contact);
+                }
+            }
+        }
     }
 }
 
