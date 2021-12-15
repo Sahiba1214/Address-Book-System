22ccb4d91dcd4de39403546d2ba895473566bfd9
sahiba <sahiba1214@gmail.com>
2021-12-15 22:08:27 +0530

    [Sahiba1214] file added Add

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -4,6 +4,8 @@ namespace AddressBookSystem
 {
     class Program
     {
+        private static object AddressBook;
+
         static void Main(string[] args)
         {
             bool flag = true;
@@ -29,6 +31,22 @@ namespace AddressBookSystem
                         };
                         break;
                     case 2:
+                        Console.WriteLine("Enter the Contact Information to be Add in form of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
+                        contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
+                        AddressBook.AddContact(contact);
+                        AddressBook.Display();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
