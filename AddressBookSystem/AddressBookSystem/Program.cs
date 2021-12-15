f26065a45cfb8ac8b45abdc26659d8925d61623a
sahiba <sahiba1214@gmail.com>
2021-12-15 21:28:15 +0530

    [Sahiba1214] file added create

--- AddressBookSystem/AddressBookSystem/Program.cs
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -1,30 +1,32 @@
-﻿
-using System;
+﻿using System;
 
 namespace AddressBookSystem
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter your Choice Number to Execute the Address Program Press- 1-Contacts, 2-Add Contact, 3-Exit");
-                int choice = Convert.ToInt32(Console.ReadLine());
-                switch (choice)
+                Console.WriteLine("Welcome to the Address Book Program");
+                Console.WriteLine("Enter what you want to do : \n 1. Create Contacts \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
                 {
                     case 1:
-                        Console.WriteLine("Enter the Contact Information in Format of FirstName, LastName, Addr, City, State, Zip, Ph.No, Email.");
-                        StartContact contact = new StartContact();
-                        contact.FirstName = Console.ReadLine();
-                        contact.LastName = Console.ReadLine();
-                        contact.Address = Console.ReadLine();
-                        contact.City = Console.ReadLine();
-                        contact.State = Console.ReadLine();
-                        contact.Zip = Console.ReadLine();
-                        contact.PhoneNumber = Console.ReadLine();
-                        contact.Email = Console.ReadLine();
+                        Console.WriteLine("Enter the Contact details of FirstName, LastName, Address, City, State, Zip, Ph.no, Email : ");
+                        Contact contact = new Contact()
+                        {
+                            FirstName = Console.ReadLine(),
+                            LastName = Console.ReadLine(),
+                            Address = Console.ReadLine(),
+                            City = Console.ReadLine(),
+                            State = Console.ReadLine(),
+                            Zip = Console.ReadLine(),
+                            PhoneNumber = Console.ReadLine(),
+                            Email = Console.ReadLine(),
+                        };
                         break;
                     case 2:
                         flag = false;
